459f40687e8e67874337afa5aafed3a89fe03d4c
lealemb <lealemb84@gmail.com>
2024-10-22 14:13:56 +0300

    Nav css added

--- src/Nav.js
+++ src/Nav.js
@@ -1,15 +1,15 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { MovieContext } from "./MovieContext";
+import './Nav.css';  // Assuming you have a CSS file
 
-const Nav =() => {
-    const [movies, setMovies] = useContext(MovieContext);
-    return (
-        <nav>
-            <h3> Dev Ed</h3>
-            <p>List of Movies: {movies.length} </p>
-  
-        </nav>
-    );
-
+const Nav = () => {
+  const [movies] = useContext(MovieContext);
+  return (
+    <nav className="navbar">
+      <h3 className="title">Dev Ed</h3>
+      <p className="movie-list">List of Movies: {movies.length}</p>
+    </nav>
+  );
 };
+
 export default Nav;
